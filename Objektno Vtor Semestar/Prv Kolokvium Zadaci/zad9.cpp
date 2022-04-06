13f1900849c824e7060cfcef8d9d2f493c484dae
Damjan Boshkovski <damjanboskovski2002@gmail.com>
2022-04-06 15:57:24 +0200

    Update zad9.cpp

--- Objektno Vtor Semestar/Prv Kolokvium Zadaci/zad9.cpp
+++ Objektno Vtor Semestar/Prv Kolokvium Zadaci/zad9.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
-struct Laptop{ char firm[100]; float size; int touch, price; };
+struct Laptop{ char firm[100]; float screenSize; bool hasTouch; int price; };
+struct ITStore{ char storeName[100], location[100]; Laptop l[100]; int num; };
 
-struct ITStore{ char store_Name[100]; char location[100]; Laptop l[100]; int num;};
-
-void print(ITStore *arr,int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i].store_Name<<" "<<arr[i].location<<endl;
-        for(int j=0;j<arr[i].num;j++){
-            arr[i].l[j].size == (int)arr[i].l[j].size?cout<<arr[i].l[j].firm<<" "<<(int) arr[i].l[j].size<<" "<<arr[i].l[j].price<<endl: cout<<arr[i].l[j].firm<<" "<<arr[i].l[j].size<<" "<<arr[i].l[j].price<<endl;
-        }
-    }
+void print(ITStore *arr, int n){
+    for(int i=0; i<n; i++){ cout<<arr[i].storeName<<" "<<arr[i].location<<endl;
+        for(int j=0; j<arr[i].num; j++){
+            if( arr[i].l[j].screenSize == (int) arr[i].l[j].screenSize ){ cout<<arr[i].l[j].firm<<" "<<(int) arr[i].l[j].screenSize<<" "<<arr[i].l[j].price<<endl; }
+            else cout<<arr[i].l[j].firm<<" "<<arr[i].l[j].screenSize<<" "<<arr[i].l[j].price<<endl; } }
 }
 
-void najeftina_ponuda(ITStore *arr, int n){
-    int min=0; int temp;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<arr[i].num;j++){
-            if(arr[i].l[j].touch == 1){  min>arr[i].l[j].price || min == 0?min= arr[i].l[j].price,temp=i:0;}
-        }
-    }
-    cout<<"Najeftina ponuda ima prodavnicata:"<<endl<<arr[temp].store_Name<<" "<<arr[temp].location<<endl<<"Najniskata cena iznesuva: "<<min;
+void cheapest_store(ITStore *arr, int n){
+    int minPrice=0, pos=0;
+    for(int i=0; i<n; i++){ for(int j=0; j<arr[i].num; j++){ 
+        arr[i].l[j].hasTouch && (minPrice > arr[i].l[j].price || minPrice == 0)? minPrice = arr[i].l[j].price, pos = i:0; } }
+    cout<<"Najeftina ponuda ima prodavnicata:"<<endl<<arr[pos].storeName<<" "<<arr[pos].location<<endl<<"Najniskata cena iznesuva: "<<minPrice;
 }
 
 int main(){
-    ITStore s[100];int n; cin>>n;
-    for (int i = 0; i < n; i++){
-        cin>>s[i].store_Name>>s[i].location>>s[i].num;
-        for (int j = 0; j < s[i].num; j++){
-            cin>>s[i].l[j].firm>>s[i].l[j].size>>s[i].l[j].touch>>s[i].l[j].price;
-        }
-    }
-    print(s, n);najeftina_ponuda(s, n);
-
+    int num; cin>>num; ITStore store[100];
+    for(int i=0; i<num; i++){ cin>>store[i].storeName>>store[i].location>>store[i].num;
+        for(int j=0; j<store[i].num; j++){ cin>>store[i].l[j].firm>>store[i].l[j].screenSize>>store[i].l[j].hasTouch>>store[i].l[j].price; } }
+    print(store, num); cheapest_store(store, num);
     return 0;
 }
