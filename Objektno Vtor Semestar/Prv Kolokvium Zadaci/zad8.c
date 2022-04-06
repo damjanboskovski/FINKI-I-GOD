4fc1816fc3148b62993a82e896bdab6693baf2d5
Damjan Boshkovski <damjanboskovski2002@gmail.com>
2022-04-06 15:07:35 +0200

    Update zad8.c

--- Objektno Vtor Semestar/Prv Kolokvium Zadaci/zad8.c
+++ Objektno Vtor Semestar/Prv Kolokvium Zadaci/zad8.c
@@ -1,38 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Laptop{ char firm[100]; float size; int touch, price; } Laptop;
-typedef struct ITStore{ char store_Name[100]; char location[100]; Laptop l[100]; int num; } ITStore;
+typedef struct Laptop{ char firm[100]; float size; int hasTouch, price}l;
+typedef struct ITStore{ char storeName[100], location[100]; l arr[100]; int num; }its;
 
-void print(ITStore *arr,int n){
-    for(int i=0;i<n;i++){ 
-        printf("%s %s\n",arr[i].store_Name,arr[i].location);
-        for(int j=0;j<arr[i].num;j++){
-            arr[i].l[j].size == (int)arr[i].l[j].size?printf("%s %d %d\n",arr[i].l[j].firm, (int) arr[i].l[j].size, arr[i].l[j].price):printf("%s %.1f %d\n", arr[i].l[j].firm, arr[i].l[j].size, arr[i].l[j].price);
-        }
-    }
+void print(its *a, int n)
+{
+    for(int i=0; i<n; i++){ printf("%s %s\n",a[i].storeName, a[i].location);
+        for(int j=0; j<a[i].num; j++){
+            if(a[i].arr[j].size == (int)a[i].arr[j].size){ printf("%s %d %d\n",a[i].arr[j].firm, (int)a[i].arr[j].size, a[i].arr[j].price); }
+            else { printf("%s %.1f %d\n",a[i].arr[j].firm, a[i].arr[j].size, a[i].arr[j].price); } } }
 }
 
-void najeftina_ponuda(ITStore *arr, int n){
-    int min=0; int temp;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<arr[i].num;j++){
-            if(arr[i].l[j].touch == 1){ min>arr[i].l[j].price || min == 0?min= arr[i].l[j].price,temp=i:0; }
-        }
-    }
-    printf("Najeftina ponuda ima prodavnicata:\n%s %s\nNajniskata cena iznesuva: %d",arr[temp].store_Name,arr[temp].location,min);
+void cheapest_store(its *a, int n)
+{
+    int min=0,pos=0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<a[i].num; j++) {
+            if(a[i].arr[j].hasTouch == 1 && (min>a[i].arr[j].price || min == 0)) { min = a[i].arr[j].price; pos = i; } } }
+    printf("Najeftina ponuda ima prodavnicata:\n%s %s\nNajniskata cena iznesuva: %d",a[pos].storeName, a[pos].location,min);
 }
 
-int main(){
-    ITStore s[100]; int n; scanf("%d", &n);
+int main()
+{
+    int num; scanf("%d", &num); its store[100];
 
-    for (int i = 0; i < n; i++){
-        scanf("%s %s %d", s[i].store_Name, s[i].location, &s[i].num);
-        for (int j = 0; j < s[i].num; j++){
-            scanf("%s %f %d %d", s[i].l[j].firm, &s[i].l[j].size, &s[i].l[j].touch ,&s[i].l[j].price);
-        }
-    }
-    print(s, n); najeftina_ponuda(s, n);
-
+    for(int i=0; i<num; i++){ scanf("%s %s %d",store[i].storeName, store[i].location, &store[i].num);
+        for(int j=0; j<store[i].num; j++){ scanf("%s %f %d %d",store[i].arr[j].firm, &store[i].arr[j].size, &store[i].arr[j].hasTouch, &store[i].arr[j].price); } }
+    print(store, num); cheapest_store(store, num);
     return 0;
 }
