c540850de874668ee818b48940dd327d477315cd
Damjan Boshkovski <damjanboskovski2002@gmail.com>
2022-11-06 19:56:43 +0100

    Update zad7.c

--- Strukturno Prv Semestar/Laboratoriski Vezbi/labs2/zad7.c
+++ Strukturno Prv Semestar/Laboratoriski Vezbi/labs2/zad7.c
@@ -2,10 +2,7 @@
 
 int main(){
     int num,trans; scanf("%d",&num);
-    for(int i=0;i<5;i++){
-        scanf("%d",&trans); num = num + trans;
-    }
-    if(num>0){printf("1");}
-    else printf("0");
+    for(int i=0;i<5;i++){ scanf("%d",&trans); num += trans; }
+    printf("%d", num>0);
     return 0;
 }
